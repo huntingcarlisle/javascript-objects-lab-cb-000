b4d36b2c76c798463a2ffdd18497ddd4c07786fa
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-03 21:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  // return Object.assign({}, object, { [key]: value });
-  object.key=value;
-  return object;
+  return Object.assign({}, object, { [key]: value });
 };
